83d011aa9f60595038acb6f2a4f23d6e39c8d1c3
JanGaud <JanisGaudreault.jg@gmail.com>
2024-03-21 17:02:51 -0400

    Loading page

---
+++ src/lib/components/ProgressBar.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { PrismicImage } from '@prismicio/svelte';
+    import { page } from '$app/stores';
+
+    const logo = $page.data.settings.data.square_logo;
+    
+    /**
+	 * @type {any}
+	 */
+     export let progress;
+    /**
+	 * @type {any}
+	 */
+     export let isLoading;
+
+    /**
+	 * @type {HTMLDivElement}
+	 */
+    let progressBarElement; // Reference to the progress bar DOM element
+    $: if (progressBarElement && $progress !== undefined) {
+        progressBarElement.style.width = `${$progress}%`;
+    }
+</script>
+
+<section class={isLoading ? 'fixed top-0 left-0 w-full h-screen z-10' : 'hidden'}>
+    <div class="fixed top-0 left-0 w-full h-2 bg-saffron-300">
+        <div bind:this={progressBarElement} class="h-full bg-saffron"></div>
+    </div>
+    <div class="flex items-center justify-center h-full w-full bg-davys_gray-100">
+        <div class="w-[300px] animate-pulse aspect-square overflow-hidden rounded-full p-10 border-4 border-saffron">
+            <PrismicImage field={logo} class="w-full h-auto object-contain" />
+        </div>
+    </div>
+</section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,62 @@
 <script>
-	import '../app.css';
-	import { PrismicPreview } from '@prismicio/svelte/kit';
-	import { page } from '$app/stores';
-	import { repositoryName } from '$lib/prismicio';
-	import Navigation from '$lib/components/Navigation.svelte';
-	import ThemeToggle from '$lib/components/ThemeToggle.svelte';
-	import MouseFxBg from '$lib/components/MouseFxBg.svelte';
+    import ProgressBar from '$lib/components/ProgressBar.svelte';
+    import '../app.css';
+    import { PrismicPreview } from '@prismicio/svelte/kit';
+    import { page } from '$app/stores';
+    import { repositoryName } from '$lib/prismicio';
+    import Navigation from '$lib/components/Navigation.svelte';
+    import ThemeToggle from '$lib/components/ThemeToggle.svelte';
+    import MouseFxBg from '$lib/components/MouseFxBg.svelte';
+    import { onMount } from 'svelte';
+    import { writable } from 'svelte/store';
+
+    let isLoading = true;
+    let progress = writable(0);
+
+    onMount(() => {
+        let intervalTime = 100;
+        let totalTime = 2200;
+        let increment = (intervalTime / totalTime) * 100;
+
+        const interval = setInterval(() => {
+            progress.update(n => {
+                if (n >= 100) {
+                    clearInterval(interval);
+                    isLoading = false;
+                    return 100;
+                }
+                return n + increment;
+            });
+        }, intervalTime);
+
+        setTimeout(() => {
+            isLoading = false;
+            progress.set(100);
+        }, totalTime);
+    });
 </script>
 
 <svelte:head>
-	<title>{$page.data.title}</title>
-	{#if $page.data.meta_description}
-		<meta name="description" content={$page.data.meta_description} />
-	{/if}
-	{#if $page.data.meta_title}
-		<meta name="og:title" content={$page.data.meta_title} />
-	{/if}
-	{#if $page.data.meta_image}
-		<meta name="og:image" content={$page.data.meta_image.url} />
-		<meta name="twitter:card" content="summary_large_image" />
-	{/if}
+    <title>{$page.data.title}</title>
+    {#if $page.data.meta_description}
+        <meta name="description" content={$page.data.meta_description} />
+    {/if}
+    {#if $page.data.meta_title}
+        <meta name="og:title" content={$page.data.meta_title} />
+    {/if}
+    {#if $page.data.meta_image}
+        <meta name="og:image" content={$page.data.meta_image.url} />
+        <meta name="twitter:card" content="summary_large_image" />
+    {/if}
 </svelte:head>
 
-<Navigation />
-<ThemeToggle />
-<main class="bg-white dark:bg-davys_gray-100 min-h-screen pt-32 pb-16 px-4 lg:px-28">
-	<slot />
-</main>
-<MouseFxBg />
+<ProgressBar {isLoading} {progress} />
+{#if !isLoading}
+    <Navigation />
+    <ThemeToggle />
+    <main class="bg-white dark:bg-davys_gray-100 min-h-screen pt-32 pb-16 px-4 lg:px-28">
+        <slot />
+    </main>
+    <MouseFxBg />
+{/if}
 <PrismicPreview {repositoryName} />
