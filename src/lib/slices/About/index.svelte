a674ee0133e2b0fe6c921b9272238e428fe29f37
JanGaud <JanisGaudreault.jg@gmail.com>
2024-03-30 16:50:10 -0400

    animated h2

--- src/lib/slices/About/index.svelte
+++ src/lib/slices/About/index.svelte
@@ -2,21 +2,35 @@
 	import Spotlight from '$lib/components/decorations/Spotlight.svelte';
 	import { PrismicRichText } from '@prismicio/svelte';
 	import { onMount } from 'svelte';
-	import gsap from 'gsap';
 
 	/** @type {import("@prismicio/client").Content.AboutSlice} */
 	export let slice;
 
 	/**
-	 * @type {gsap.TweenTarget}
+	 * @type {HTMLElement}
 	 */
 	let whatIDoRef;
 
-	onMount(() => {
-		gsap.timeline().fromTo(
+	onMount(async () => {
+		// Dynamic imports for GSAP and its plugins
+		const gsapModule = await import('gsap');
+		const { ScrollTrigger } = await import('gsap/ScrollTrigger');
+
+		// Register ScrollTrigger plugin
+		gsapModule.gsap.registerPlugin(ScrollTrigger);
+
+		// Now, you can use gsap and ScrollTrigger
+		gsapModule.gsap.fromTo(
 			whatIDoRef,
 			{ y: 20, opacity: 0, scale: 1.2 },
 			{
+				scrollTrigger: {
+					trigger: whatIDoRef,
+					start: 'top center',
+					end: 'center center',
+
+					toggleActions: 'play none none reverse'
+				},
 				opacity: 1,
 				y: 0,
 				scale: 1,
@@ -27,6 +41,7 @@
 	});
 </script>
 
+
 <section
 	class="relative mb-10 lg:mb-20 text-center md:text-left flex justify-center"
 	data-slice-type={slice.slice_type}
@@ -34,17 +49,18 @@
 >
 	<Spotlight width="400px" position="left-3 top-0" rgb="45, 125, 210" />
 	<Spotlight width="325px" position="bottom-3" rgb="244, 93, 1" />
-	<div class="z-20 w-full flex flex-wrap flex-col items-center gap-4 cursor-none pointer-events-none">
+	<div
+		class="z-20 w-full flex flex-wrap flex-col items-center gap-4 cursor-none pointer-events-none"
+	>
 		<h2
 			bind:this={whatIDoRef}
 			class="font-bold tracking-tighter mb-4 text-4xl lg:text-6xl drop-shadow-lg w-fit text-center border-b-8 border-saffron"
 		>
 			{slice.primary.title}
 		</h2>
-		<div class="">
-			<div class="overflow-hidden">
 
-			</div>
+		<div class="flex flex-wrap justify-center items-center gap-6 lg:grid lg:grid-cols-2">
+			<div class="h-[300px] w-full md:h-full border"></div>
 			<div class="flex flex-col gap-4">
 				<PrismicRichText field={slice.primary.text} />
 			</div>
