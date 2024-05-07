1187ad4aa7e752e251c9703b63ee8c13fc2ac305
JanGaud <JanisGaudreault.jg@gmail.com>
2024-05-07 18:56:18 -0400

    small meteo widget fix

--- src/lib/components/meteo/MeteoSkeleton.svelte
+++ src/lib/components/meteo/MeteoSkeleton.svelte
@@ -1,9 +1,7 @@
 <script>
 	import CardCarrouselSkeleton from '$lib/components/meteo/CardCarrouselSkeleton.svelte';
-	import SnowCountSkeleton from '$lib/components/meteo/SnowCountSkeleton.svelte';
 	import MeteoInfoSkeleton from './MeteoInfoSkeleton.svelte';
 </script>
 
-<SnowCountSkeleton/>
 <MeteoInfoSkeleton/>
 <CardCarrouselSkeleton/>
--- src/lib/components/meteo/SnowCountSkeleton.svelte
+++
@@ -1,6 +0,0 @@
-<div class="p-2 mb-2 flex items-center gap-2 border rounded-md shadow-md text-sm animate-pulse">
-	<div class="w-6 h-6 bg-gray-300 rounded-full"></div>
-	<div class="flex-1 space-y-2">
-		<div class="h-4 bg-gray-300 rounded"></div>
-	</div>
-</div>
