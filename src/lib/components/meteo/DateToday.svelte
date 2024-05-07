395209009aa738d71fa31ec19c9b242fdcfa6d1b
JanGaud <JanisGaudreault.jg@gmail.com>
2024-05-07 18:36:32 -0400

    small meteo widget fix

--- src/lib/components/meteo/DateToday.svelte
+++ src/lib/components/meteo/DateToday.svelte
@@ -1,17 +1,19 @@
-<script>  
-    $: today = new Date().toLocaleDateString("fr-CA");  
+<script>
+    // Get the local date
+    $: today = new Date();
 
-    	/**
-	 * @param {string | number | Date} dateTimeTxt
-	 */
-	function formatDate(dateTimeTxt) {
-		return new Date(dateTimeTxt).toLocaleDateString('fr-CA', {
-			weekday: 'long',
-			month: 'long',
-			day: 'numeric'
-		});
-	}
-</script>  
-<div class="absolute font-bold bottom-2 right-4 uppercase">  
-    <p class="text-[12px] {$$props["class"]}">{formatDate(today)}</p>  
-</div>  
+    /**
+     * Formats the given date or date string into a human-readable format in the French Canadian locale.
+     * @param {string | number | Date} dateTimeTxt
+     */
+    function formatDate(dateTimeTxt) {
+        return new Date(dateTimeTxt).toLocaleDateString('fr-CA', {
+            weekday: 'long',
+            month: 'long',
+            day: 'numeric'
+        });
+    }
+</script>
+<div class="absolute font-bold bottom-2 right-4 uppercase">
+    <p class="text-[14px] tracking-wider {$$props["class"]}">{formatDate(today)}</p>
+</div>
