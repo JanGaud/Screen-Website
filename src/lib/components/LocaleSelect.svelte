7e2521dfd7f238f34d9ed0d2753938f9eba7b28d
JanGaud <JanisGaudreault.jg@gmail.com>
2024-05-08 16:00:28 -0400

    locale toggle front-end done

--- src/lib/components/LocaleSelect.svelte
+++ src/lib/components/LocaleSelect.svelte
@@ -1,31 +1,23 @@
 <!-- LocaleSelect.svelte -->
 <script lang="ts">
-    import { writable } from 'svelte/store';
+	import type { ChangeEventHandler } from 'svelte/elements';
+	import { selectedLocale } from '../../stores/localeStore';
 
-    export const selectedLocale = writable('fr-CA');
-
-    const changeLocale = (event: Event) => {
-        const target = event.target as HTMLSelectElement;
-        const locale = target.value;
-        if (locale) {
-            selectedLocale.set(locale);
-            console.log($selectedLocale);
-        }
-    };
+	const changeLocale: ChangeEventHandler<HTMLSelectElement> = (event) => {
+		const target = event.target as HTMLSelectElement;
+		const locale = target.value;
+		if (locale) {
+			selectedLocale.set(locale);
+			console.log($selectedLocale);
+		}
+	};
 </script>
 
 <select
-    class="fixed bg-transparent z-30 pointer-events-auto bottom-4 right-4"
-    bind:value={$selectedLocale}
-    on:change={changeLocale}
+	class="fixed bg-transparent font-semibold tracking-widest z-30 pointer-events-auto bottom-4 right-4"
+	bind:value={$selectedLocale}
+	on:change={changeLocale}
 >
-    <option value="en-US">En</option>
-    <option value="fr-CA">Fr</option>
+	<option value="fr-CA">Fr</option>
+	<option value="en-US">En</option>
 </select>
-
-<style>
-    select option {
-        margin: 40px;
-        background: rgba(0, 0, 0, 0.459);
-    }
-</style>
