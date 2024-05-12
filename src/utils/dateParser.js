cfe3f6ef82527c657ff757b5bcb4ae61af7e45fc
JanGaud <JanisGaudreault.jg@gmail.com>
2024-05-12 13:48:57 -0400

    fixed billingual date parser

--- src/utils/dateParser.js
+++ src/utils/dateParser.js
@@ -1,7 +1,13 @@
+let lang = 'fr-ca';
+
+if (typeof window !== 'undefined') {
+    lang = window.location.pathname.startsWith('/en-us') ? 'en-us' : 'fr-ca';
+}
+
 /**
  * @param {string | number | Date} dateString
  */
-function formatDate(dateString, locale = 'fr-FR') {
+function formatDate(dateString, locale = lang) {
     const date = new Date(dateString);
     const formatter = new Intl.DateTimeFormat(locale, {
         year: 'numeric',
