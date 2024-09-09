74de2d27c391333117704244cc966e47576eb67b
JanGaud <JanisGaudreault.jg@gmail.com>
2024-09-09 11:35:07 -0400

    Clock changes not just on reload

--- src/lib/components/meteo/Clock.svelte
+++ src/lib/components/meteo/Clock.svelte
@@ -1,4 +1,22 @@
-<script>  
-    $: now = new Date().toLocaleTimeString("en-CA", { hour: '2-digit', minute: '2-digit' });  
-</script>  
-<time class="text-xl md:text-3xl {$$props["class"]}">{now}</time>  
+<script>
+    import { onDestroy } from 'svelte';
+
+    /**
+	 * @type {string}
+	 */
+    let now;
+
+    function updateTime() {
+        now = new Date().toLocaleTimeString("en-CA", { hour: '2-digit', minute: '2-digit' });
+    }
+
+    updateTime();
+
+    const interval = setInterval(updateTime, 1000);
+
+    onDestroy(() => {
+        clearInterval(interval);
+    });
+</script>
+
+<time class="text-xl md:text-3xl {$$props["class"]}">{now}</time>
